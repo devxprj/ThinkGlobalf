4c2329bd349e9b0d4fe01b7d2629ff6959ecfb39
fadl <135347478+devxprj@users.noreply.github.com>
2025-12-28 00:22:41 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,13 +1,9 @@
 import mysql from "mysql2";
 
-export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "", 
-  database: "lms"
-});
-
-db.connect(err => {
-  if (err) throw err;
-  console.log("Connected to MySQL database");
+export const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
